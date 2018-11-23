c9383e80b8f69f1fecbcffa4c440cb1edf36690f
schcilin <3144395995@qq.com>
2018-11-23 18:29:13 +0800

    java8实现

--- redisModule/src/main/java/com/schcilin/redismodule/controller/UserOrderController.java
+++ redisModule/src/main/java/com/schcilin/redismodule/controller/UserOrderController.java
@@ -25,29 +25,27 @@ public class UserOrderController {
     public String order() {
         String key = "redisson_key";
         for (int i = 0; i < 100; i++) {
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        boolean isGetLock = distributedLock.tryLock(key, TimeUnit.SECONDS, 5L, 10L); //尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
-                        Future<Boolean> lockAsync = distributedLock.tryLockAsync(key, TimeUnit.SECONDS, 5L, 10L);//尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
-                        if (lockAsync.get()) {
-                            try {
+            //java8Lambda表达式
+            new Thread(() -> {
+                try {
+                    // boolean isGetLock = distributedLock.tryLock(key, TimeUnit.SECONDS, 5L, 10L); //尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
+                    Future<Boolean> lockAsync = distributedLock.tryLockAsync(key, TimeUnit.SECONDS, 5L, 10L);//尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
+                    if (lockAsync.get()) {
+                        try {
 
-                                Thread.sleep(100); //获得锁之后可以进行相应的处理
-                                System.err.println("======获得锁后进行相应的操作======" + Thread.currentThread().getName());
-                            } finally {
+                            Thread.sleep(100); //获得锁之后可以进行相应的处理
+                            log.info("======获得锁后进行相应的操作======" + Thread.currentThread().getName());
+                        } finally {
 
-                                distributedLock.unLock(key);
-                            }
-                            System.err.println("=============================" + Thread.currentThread().getName());
+                            distributedLock.unLock(key);
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        log.info("=============================" + Thread.currentThread().getName());
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            });
-            t.start();
+
+            }).start();
         }
         return null;
     }
