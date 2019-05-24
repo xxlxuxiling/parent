b29f6613325fa41c0d3f252dc8e3cb710615904a
xuxiling <xuxl@anfanet.com>
2019-05-24 18:14:32 +0800

    消息事务

---
+++ job-server/src/test/java/com/schcilin/jobserver/JobServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.schcilin.jobserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ pay-server/src/main/java/com/schcilin/payserver/PayServerApplication.java
@@ -0,0 +1,13 @@
+package com.schcilin.payserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayServerApplication.class, args);
+    }
+
+}
