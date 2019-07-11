9e85e7a7eb89e8048e4a3b3b8515eb22491cb365
xuxiling <xuxl@anfanet.com>
2019-07-11 20:21:54 +0800

    自定义配置负载均衡策略

--- pay-server/src/main/java/com/schcilin/payserver/strategy/impl/ICBCBankImpl.java
+++ pay-server/src/main/java/com/schcilin/payserver/strategy/impl/ICBCBankImpl.java
@@ -1,5 +1,8 @@
 package com.schcilin.payserver.strategy.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.schcilin.payserver.entity.PayChannel;
 import com.schcilin.payserver.mapper.PayChannelMapper;
 import com.schcilin.payserver.strategy.Pay;
@@ -24,6 +27,12 @@ public class ICBCBankImpl extends ProjectBeanUtils implements PayStrategy {
     @Override
     public BigDecimal calculateRecharge(String channelId, String goodsId) {
         PayChannel payChannel = payChannelMapper.selectById(channelId);
+        IPage page = new Page(1,1);
+        QueryWrapper<PayChannel> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(PayChannel::getName,"sss");
+
+        IPage iPage = payChannelMapper.selectPage(page, queryWrapper);
+
         return null;
     }
 }
