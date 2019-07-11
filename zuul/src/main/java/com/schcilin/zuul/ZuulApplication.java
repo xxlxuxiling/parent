5a558c0d345d5d0d152d0e03299dd72a178741f9
xuxiling <xuxl@anfanet.com>
2019-07-11 20:23:06 +0800

    自定义配置负载均衡策略

--- zuul/src/main/java/com/schcilin/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/schcilin/zuul/ZuulApplication.java
@@ -3,11 +3,14 @@ package com.schcilin.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
 
 /**
- zuul网关代理,可用作限流
+ * zuul网关代理,可用作限流
  */
 @SpringCloudApplication
 @EnableZuulProxy
@@ -17,6 +20,15 @@ public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    /**
+     * 开启负载均衡
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
